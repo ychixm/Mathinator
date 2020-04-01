c599148bd73ca9cd67882cc4b1ecc2edd2c1cbf6
Ychixm <yochix.M@hotmail.fr>
2020-04-01 15:42:57 +0200

    test2

--- Calc/Calc.java
+++ Calc/Calc.java
@@ -1,4 +1,18 @@
 package Calc;
 
 public class Calc {
+    public Calc(){
+
+    }
+
+    int a;
+    int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
 }
