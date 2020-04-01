cba432212876d99aa7653ea20bb905ddc920d51e
Ychixm <yochix.M@hotmail.fr>
2020-04-01 15:45:37 +0200

    test3

--- Calc/Calc.java
+++ Calc/Calc.java
@@ -1,18 +1,4 @@
 package Calc;
 
 public class Calc {
-    public Calc(){
-
-    }
-
-    int a;
-    int b;
-
-    public int getA() {
-        return a;
-    }
-
-    public int getB() {
-        return b;
-    }
 }
